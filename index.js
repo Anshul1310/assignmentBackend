fe6d45e585646de0ffb690373b18b3c88f47f256
Anshul <negi.anshulnegi17@gmail.com>
2025-10-25 18:05:10 +0530

    hj

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const Event=require("./models/Event");
 
 app.use(express.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'none'; connect-src 'self' https://assignmentbackend-ty4p.onrender.com;" // Add connect-src here
+    // Add other directives separated by semicolons
+    // Example: "script-src 'self';"
+  );
+  next();
+});
 mongoose.connect("mongodb+srv://anshul:anshul@indulge.jhz3dxr.mongodb.net/",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
