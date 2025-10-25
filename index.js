52fd44f1b5e26e9b7cfe67d0eef2a50312fa4cdc
Anshul <negi.anshulnegi17@gmail.com>
2025-10-25 15:01:04 +0530

    correction

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/events/get",async (req,res)=>{
 
 
 app.get("/hello",(req,res)=>{
-  res.status(200).json("success");
+  res.status(200).json("success2");
 })
 
 // POST endpoint to create an event
